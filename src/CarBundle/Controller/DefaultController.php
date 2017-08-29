1e6c1b3141e572ca651445e11a21ad4ece98e56e
Sergey Danilkin <danilkin.sergey@gmail.com>
2017-08-29 17:39:29 +0300

    finish

--- src/CarBundle/Controller/DefaultController.php
+++ src/CarBundle/Controller/DefaultController.php
@@ -2,19 +2,56 @@
 
 namespace CarBundle\Controller;
 
+use CarBundle\CarBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/our-cars", name="offer")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        $cars = [
-            ['make' => 'BMW', 'name' => 'X1']
-        ];
-        return $this->render('CarBundle:Default:index.html.twig',['cars' => $cars]);
+        $carRepository = $this->getDoctrine()->getRepository('CarBundle:Car');
+        $cars = $carRepository->findCarsWithDetails();
+
+        $form = $this->createFormBuilder()
+            ->setMethod('GET')
+            ->add('search', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 2])
+                ]])
+            ->add('submit', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            die();
+        }
+
+        return $this->render('CarBundle:Default:index.html.twig',
+            [
+                'cars' => $cars,
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     * @Route("/car/{id}", name="show_car")
+     */
+    public function showAction($id)
+    {
+        $carRepository = $this->getDoctrine()->getRepository('CarBundle:Car');
+        $car = $carRepository->findCarWithDetailsById($id);
+        return $this->render('CarBundle:Default:show.html.twig', ['car' => $car]);
     }
 }
---
+++ src/CarBundle/Twig/Extension/show.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CarBundle\Twig\Extension;
+
+class show extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'show';
+    }
+}
